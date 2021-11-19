283287c80259b18635f6db01ee7ced00894ba7d3
DurandEtienne <etienne1.durand@gmail.com>
2021-11-19 10:06:28 +0100

    ajout des fonctions de lecture du fichier de maillage cpp et h

---
+++ lecture.h
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <math.h>
+#include <ctime>
+#include <fstream>
+#include <string>
+#include<vector>
+
+std::vector<std::vector<double>> lecture(std::string nomdufichier);
